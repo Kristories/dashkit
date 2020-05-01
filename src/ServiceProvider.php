7a619916e7e4601b3100175a0e66d496fdec36b0
Wahyu Kristianto <w.kristories@gmail.com>
2020-05-01 20:56:52 +0700

    Create ServiceProvider.php

---
+++ src/ServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kristories\Dashkit;
+
+use Illuminate\Support\Facades\Route;
+use Kristories\Dashkit\Console\InstallCommand;
+use Kristories\Dashkit\Console\MakeTileCommand;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+}
