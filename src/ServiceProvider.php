28747b6d518f83dc11b1805c1379cb816e42536c
Wahyu Kristianto <w.kristories@gmail.com>
2020-05-01 20:58:34 +0700

    Setup config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,4 +8,34 @@ use Kristories\Dashkit\Console\MakeTileCommand;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    public function register()
+    {
+        $this->configure();
+    }
+
+    /**
+     * Setup the configuration for Dashkit.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/dashkit.php', 'dashkit'
+        );
+    }
+
+    /**
+     * Setup the resource publishing groups for Dashkit.
+     *
+     * @return void
+     */
+    protected function offerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/dashkit.php' => config_path('dashkit.php'),
+            ], 'dashkit-config');
+        }
+    }
 }
